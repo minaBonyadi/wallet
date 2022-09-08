10cfb41c074dfb8a72755af067c6bbca3f9b1462
mina <mina.bonyadi92@gmail.com>
2022-09-08 12:03:16 +0430

    integration test and unit test

--- src/main/java/com/leovegas/wallet/service/WalletService.java
+++ src/main/java/com/leovegas/wallet/service/WalletService.java
@@ -27,20 +27,20 @@ public class WalletService {
     private final TransactionRepository transactionRepository;
     private final TransactionStrategyFactory transactionStrategyFactory;
 
-    /**
+    /** fetch a player data by id
      *
-     * @param playerId
-     * @return
+     * @param playerId long player id
+     * @return a player data
      */
     public Player getPlayerById(long playerId) {
         return playerRepository.findById(playerId).orElseThrow(() -> new NotFoundException("Player not found!"));
     }
 
-    /**
+    /** create a transaction for a player and run it base on their type which handled by Strategy pattern
      *
-     * @param playerId
-     * @param transactionDto
-     * @return
+     * @param playerId long player id
+     * @param transactionDto new transaction request from client
+     * @return rest response which is including code and message
      */
 	@Transactional
     public RestResponse createAndRunTransaction(Long playerId, TransactionDto transactionDto) {
@@ -55,7 +55,13 @@ public class WalletService {
 		return new RestResponse(RestResponseType.SUCCESS, "The transaction has just done!");
     }
 
-	public PlayerTransaction createTransaction(Long playerId, TransactionDto transactionDto) {
+	/** create a transaction and save on database
+	 *
+	 * @param playerId long player id
+	 * @param transactionDto new transaction request from client
+	 * @return created transaction to run by createAndRunTransaction()
+	 */
+	private PlayerTransaction createTransaction(Long playerId, TransactionDto transactionDto) {
 		PlayerTransaction playerTransaction = PlayerTransaction.builder()
 				.player(playerRepository.findById(playerId).orElseThrow(() ->
 						new NotFoundException("Player not found!")))
@@ -66,10 +72,10 @@ public class WalletService {
 		return transactionRepository.save(playerTransaction);
 	}
 
-    /**
+    /** fetch all transactions of a player by id
      *
-     * @param playerId
-     * @return
+     * @param playerId long player id
+     * @return transactions list of a player
      */
     public List<PlayerTransaction> getPlayerTransactions(long playerId) {
         return playerRepository.findById(playerId).orElseThrow(()-> new NotFoundException("Player not found!"))
